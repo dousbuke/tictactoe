4562534081ca44b0a0b967b9eb11e9baec838e70
dbuke <dogus.buke@eftsoftware.com>
2017-11-04 17:23:19 +0300

    final changes

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -64,12 +64,108 @@ public class Board {
                 return checkFor3(players);
             case 4:
                 return checkFor4(players);
+            case 5:
+                return checkFor5(players);
+            case 6:
+                return checkFor6(players);
+            case 7:
+                return checkFor7(players);
+            case 8:
+                return checkFor8(players);
+            case 9:
+                return checkFor9(players);
+            case 10:
+                return checkFor10(players);
             default:
                 System.out.println("Invalid input");
                 break;
         }
 
         return false;
+    }
+
+    private boolean checkFor10(Players players) {
+        return (cells[currentRow][0].players == players && cells[currentRow][1].players == players && cells[currentRow][2].players == players
+                    && cells[currentRow][3].players == players && cells[currentRow][4].players == players && cells[currentRow][5].players == players
+                    && cells[currentRow][6].players == players && cells[currentRow][7].players == players && cells[currentRow][8].players == players
+                    && cells[currentRow][9].players == players
+                || cells[0][currentCol].players == players && cells[1][currentCol].players == players && cells[2][currentCol].players == players
+                    && cells[3][currentCol].players == players && cells[4][currentCol].players == players && cells[5][currentCol].players == players
+                    && cells[6][currentCol].players == players && cells[7][currentCol].players == players && cells[8][currentCol].players == players
+                    && cells[9][currentCol].players == players
+                || currentRow == currentCol && cells[0][0].players == players && cells[1][1].players == players && cells[2][2].players == players
+                    && cells[3][3].players == players && cells[4][4].players == players && cells[5][5].players == players
+                    && cells[6][6].players == players && cells[7][7].players == players && cells[8][8].players == players
+                    && cells[9][9].players == players
+                || currentRow + currentCol == 9 && cells[0][9].players == players && cells[1][8].players == players && cells[2][7].players == players
+                    && cells[3][6].players == players) && cells[4][5].players == players && cells[5][4].players == players && cells[6][3].players == players
+                    && cells[7][2].players == players && cells[8][1].players == players && cells[9][0].players == players;
+    }
+
+    private boolean checkFor9(Players players) {
+        return (cells[currentRow][0].players == players && cells[currentRow][1].players == players && cells[currentRow][2].players == players
+                    && cells[currentRow][3].players == players && cells[currentRow][4].players == players && cells[currentRow][5].players == players
+                    && cells[currentRow][6].players == players && cells[currentRow][7].players == players && cells[currentRow][8].players == players
+                || cells[0][currentCol].players == players && cells[1][currentCol].players == players && cells[2][currentCol].players == players
+                    && cells[3][currentCol].players == players && cells[4][currentCol].players == players && cells[5][currentCol].players == players
+                    && cells[6][currentCol].players == players && cells[7][currentCol].players == players && cells[8][currentCol].players == players
+                || currentRow == currentCol && cells[0][0].players == players && cells[1][1].players == players && cells[2][2].players == players
+                    && cells[3][3].players == players && cells[4][4].players == players && cells[5][5].players == players
+                    && cells[6][6].players == players && cells[7][7].players == players && cells[8][8].players == players
+                || currentRow + currentCol == 8 && cells[0][8].players == players && cells[1][7].players == players && cells[2][6].players == players
+                    && cells[3][5].players == players) && cells[4][4].players == players && cells[5][3].players == players && cells[6][2].players == players
+                    && cells[7][1].players == players && cells[8][0].players == players;
+    }
+
+    private boolean checkFor8(Players players) {
+        return (cells[currentRow][0].players == players && cells[currentRow][1].players == players && cells[currentRow][2].players == players
+                    && cells[currentRow][3].players == players && cells[currentRow][4].players == players && cells[currentRow][5].players == players
+                    && cells[currentRow][6].players == players && cells[currentRow][7].players == players
+                || cells[0][currentCol].players == players && cells[1][currentCol].players == players && cells[2][currentCol].players == players
+                    && cells[3][currentCol].players == players && cells[4][currentCol].players == players && cells[5][currentCol].players == players
+                    && cells[6][currentCol].players == players && cells[7][currentCol].players == players
+                || currentRow == currentCol && cells[0][0].players == players && cells[1][1].players == players && cells[2][2].players == players
+                    && cells[3][3].players == players && cells[4][4].players == players && cells[5][5].players == players
+                    && cells[6][6].players == players && cells[7][7].players == players
+                || currentRow + currentCol == 7 && cells[0][7].players == players && cells[1][6].players == players && cells[2][5].players == players
+                    && cells[3][4].players == players) && cells[4][3].players == players && cells[5][2].players == players && cells[6][1].players == players
+                    && cells[7][0].players == players;
+    }
+
+    private boolean checkFor7(Players players) {
+        return (cells[currentRow][0].players == players && cells[currentRow][1].players == players && cells[currentRow][2].players == players
+                    && cells[currentRow][3].players == players && cells[currentRow][4].players == players && cells[currentRow][5].players == players
+                    && cells[currentRow][6].players == players
+                || cells[0][currentCol].players == players && cells[1][currentCol].players == players && cells[2][currentCol].players == players
+                    && cells[3][currentCol].players == players && cells[4][currentCol].players == players && cells[5][currentCol].players == players
+                    && cells[6][currentCol].players == players
+                || currentRow == currentCol && cells[0][0].players == players && cells[1][1].players == players && cells[2][2].players == players
+                    && cells[3][3].players == players && cells[4][4].players == players && cells[5][5].players == players
+                    && cells[6][6].players == players
+                || currentRow + currentCol == 6 && cells[0][6].players == players && cells[1][5].players == players && cells[2][4].players == players
+                    && cells[3][3].players == players) && cells[4][2].players == players && cells[5][1].players == players && cells[6][0].players == players;
+    }
+
+    private boolean checkFor6(Players players) {
+        return (cells[currentRow][0].players == players && cells[currentRow][1].players == players && cells[currentRow][2].players == players
+                    && cells[currentRow][3].players == players && cells[currentRow][4].players == players && cells[currentRow][5].players == players
+                || cells[0][currentCol].players == players && cells[1][currentCol].players == players && cells[2][currentCol].players == players
+                    && cells[3][currentCol].players == players && cells[4][currentCol].players == players && cells[5][currentCol].players == players
+                || currentRow == currentCol && cells[0][0].players == players && cells[1][1].players == players && cells[2][2].players == players
+                    && cells[3][3].players == players && cells[4][4].players == players && cells[5][5].players == players
+                || currentRow + currentCol == 5 && cells[0][5].players == players && cells[1][4].players == players && cells[2][3].players == players
+                    && cells[3][2].players == players) && cells[4][1].players == players && cells[5][0].players == players;
+    }
+
+    private boolean checkFor5(Players players) {
+        return (cells[currentRow][0].players == players && cells[currentRow][1].players == players && cells[currentRow][2].players == players
+                        && cells[currentRow][3].players == players && cells[currentRow][4].players == players
+                || cells[0][currentCol].players == players && cells[1][currentCol].players == players && cells[2][currentCol].players == players
+                        && cells[3][currentCol].players == players && cells[4][currentCol].players == players
+                || currentRow == currentCol && cells[0][0].players == players && cells[1][1].players == players && cells[2][2].players == players
+                        && cells[3][3].players == players && cells[4][4].players == players
+                || currentRow + currentCol == 4 && cells[0][4].players == players && cells[1][3].players == players && cells[2][2].players == players
+                        && cells[3][1].players == players) && cells[4][0].players == players;
     }
 
     private boolean checkFor4(Players players) {
